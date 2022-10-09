c74373ae9d8d7599777584ca82e0c1665b3bfbdd
nightstalker81a <115430642+nightstalker81a@users.noreply.github.com>
2022-10-09 23:16:16 +0100

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -1,5 +1,5 @@
 /*   STEP 1   */
-export const leagueID = "825504102233092096"; // your league ID
+export const leagueID = "826504102233092096"; // your league ID
 export const leagueName = "Masters of Malt"; // your league name
 export const dues = 100; // (optional) used in template constitution page
 export const dynasty = false; // true for dynasty leagues, false for redraft and keeper
